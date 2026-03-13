6c6fd79cd6eb3837e9bad229feb7ac3612c1f30f
akshat-jain-01 <akshatjainkht01@gmail.com>
2026-03-13 18:52:51 +0530

    lot_location entity added

---
+++ wms-backend/src/modules/inventory/lotLocation/entities/lot_location.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+import { Lot } from "../../lot/entities/lot.entity";
+import { Rack } from "../../rack/entities/rack.entity";
+
+@Entity('lot_locations')
+@Unique(['lotId','rackId'])
+export class LotLocation{
+
+  @PrimaryGeneratedColumn('uuid')
+  id !: string;
+
+  @Column()
+  lotId!: string;
+
+  @ManyToOne(()=> Lot)
+  @JoinColumn({name: 'lotId'})
+  lot!: Lot;
+
+  @Column()
+  rackId!: string;
+
+  @ManyToOne(()=> Rack)
+  @JoinColumn({name: 'rackId'})
+  rack!: Rack;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
