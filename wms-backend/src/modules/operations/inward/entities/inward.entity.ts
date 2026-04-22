747c951b2162b25e84e36a3c231253b631df68a6
akshat-jain-01 <akshatjainkht01@gmail.com>
2026-04-23 00:48:53 +0530

    lot and lot-location module is done

--- wms-backend/src/modules/operations/inward/entities/inward.entity.ts
+++ wms-backend/src/modules/operations/inward/entities/inward.entity.ts
@@ -1,10 +1,18 @@
 
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Warehouse } from 'src/modules/inventory/warehouse/entities/warehouse.entity';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('inward')
 export class Inward {
 
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: string;
+
+  @Column()
+  warehouseId !: string;
+
+  @ManyToOne(()=> Warehouse)
+  @JoinColumn({name: 'warehouseId'})
+  warehouse!: Warehouse;
 
 }
--- wms-backend/src/modules/partner/customer/entities/customer.entity.ts
+++ wms-backend/src/modules/partner/customer/entities/customer.entity.ts
@@ -1,10 +1,9 @@
 
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('customer')
 export class Customer {
 
-  @PrimaryGeneratedColumn()
-  id: number;
-
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 }
